af14d55014e3027cb4ab4a0c5ed702284fa26d21
hitak <hitak77@naver.com>
2023-10-27 08:56:13 +0900

    level 0) 나머지 구하기

---
+++ Integration_java/src/main/kotlin/remainder.java
@@ -0,0 +1,12 @@
+public class remainder {
+    public int solution(int num1, int num2) {
+        int answer = -1;
+        answer = num1 % num2;
+        return answer;
+    }
+}
+
+/**
+ * level 0) 나머지 구하기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120810
+ */

25debec65de4f9a546e2f4d0810feb913cfff6aa
hitak <hitak77@naver.com>
2023-10-27 08:54:38 +0900

    level 0) 숫자 비교하기

---
+++ Integration_java/src/main/kotlin/compareNumbers.java
@@ -0,0 +1,11 @@
+public class compareNumbers {
+    public int solution(int num1, int num2) {
+        return num1 == num2 ? 1 : -1;
+        //삼항연산자
+    }
+}
+
+/**
+ * level 0) 숫자 비교하기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/120807
+ */
